949fe86ba75149bd952a085e650a02bee30c4aa1
Kastrulkin <demyanovap@gmail.com>
2026-04-10 11:27:56 +0300

    Add Yandex Metrika counter 108482177

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,24 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700,800&amp;subset=cyrillic-ext' }
-    ]
+    ],
+    script: [
+      {
+        hid: 'yandex-metrika',
+        innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108482177','ym');ym(108482177,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
+        type: 'text/javascript'
+      }
+    ],
+    noscript: [
+      {
+        hid: 'yandex-metrika-noscript',
+        innerHTML: '<div><img src="https://mc.yandex.ru/watch/108482177" style="position:absolute; left:-9999px;" alt="" /></div>'
+      }
+    ],
+    __dangerouslyDisableSanitizersByTagID: {
+      'yandex-metrika': ['innerHTML'],
+      'yandex-metrika-noscript': ['innerHTML']
+    }
   },
   serverMiddleware: [
     '~/server/index.js'
